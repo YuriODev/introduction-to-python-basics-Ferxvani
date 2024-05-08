bcb60f0fbe11f33ec2493856be3aaeb196d8d536
Dawid <112199486+20Szpandd@users.noreply.github.com>
2024-05-08 14:03:31 +0000

    solve ex1

--- exercises/exercise_1.py
+++ exercises/exercise_1.py
@@ -1,2 +1,7 @@
 # Exercise 1
 # Your solution comes here
+
+input = input("enter a number: ")
+number1 = int(input[0]) + int(input[2]) + int(input[4])
+number2 = int(input[1]) + int(input[3])  
+print (str(number1)+str(number2))
